058dedaa5086f00c7db2527d8bfeb60903ab6ccf
Prabhath922 <prabhathsudarapalli922@gmail.com>
2025-09-02 16:18:28 +0530

    smaller project intitial commit

---
+++ micro-project1/index.py
@@ -0,0 +1,9 @@
+# temprature converter
+def farienhiet_to_cel(f):
+    return(f-32)*5/9
+
+def celcius_to_farientheit(c):
+    return (c*9/5)/32
+
+print(farienhiet_to_cel(20))
+print(celcius_to_farientheit(10))
